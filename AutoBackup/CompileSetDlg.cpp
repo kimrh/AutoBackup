c97076e157f00e08fe880021027c0a5c0f01cd75
RONRaehyunKim <43144446+RONRaehyunKim@users.noreply.github.com>
2018-11-29 22:46:33 +0900

    button function add

--- AutoBackup/CompileSetDlg.cpp
+++ AutoBackup/CompileSetDlg.cpp
@@ -15,6 +15,9 @@ BEGIN_MESSAGE_MAP(CCompileSetDlg, CDialog)
 	ON_BN_CLICKED(IDC_CHECKEXCEPT, &CCompileSetDlg::OnBnClickedCheckexcept)
 	ON_BN_CLICKED(IDC_CHECKEXT, &CCompileSetDlg::OnBnClickedCheckext)
 	ON_BN_CLICKED(IDC_CHECKDATE, &CCompileSetDlg::OnBnClickedCheckdate)
+	ON_BN_CLICKED(IDC_BUTTON_COMPILESRC, &CCompileSetDlg::OnBnClickedButtonCompilesrc)
+	ON_BN_CLICKED(IDC_BUTTON_COMPILECOPYSRC, &CCompileSetDlg::OnBnClickedButtonCompilecopysrc)
+	ON_BN_CLICKED(IDC_RGST_COMPILESET_BTN, &CCompileSetDlg::OnBnClickedRgstCompilesetBtn)
 END_MESSAGE_MAP()
 
 
@@ -105,4 +108,114 @@ void CCompileSetDlg::OnBnClickedCheckext()
 void CCompileSetDlg::OnBnClickedCheckdate()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void CCompileSetDlg::OnBnClickedButtonCompilesrc()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	try
+	{
+		GetDlgItem(IDC_BUTTON_COMPILESRC)->EnableWindow(FALSE);
+		BROWSEINFO bInfo;
+		bInfo.hwndOwner = GetSafeHwnd();
+		bInfo.pidlRoot = NULL;
+		bInfo.ulFlags = BIF_RETURNONLYFSDIRS;
+		memset(&bInfo, 0, sizeof(bInfo));
+
+		LPITEMIDLIST path;
+		path = SHBrowseForFolder(&bInfo);
+
+		TCHAR szpathName[300];
+		SHGetPathFromIDList(path, szpathName);
+		CString strtmp = szpathName;
+		if (strtmp != "")
+			m_strEditSRCPath = szpathName;
+		else
+			m_strEditSRCPath = "";
+
+		GetDlgItem(IDC_EDIT_SRCPATH)->SetWindowText(m_strEditSRCPath);
+		GetDlgItem(IDC_BUTTON_COMPILESRC)->EnableWindow(TRUE);
+	}
+	catch (CException *e)
+	{
+		e->Delete();
+		AfxMessageBox(_T("경로가 잘못되었습니다 다시 선택하세요."));
+		UpdateData(FALSE);
+	}
+}
+
+
+void CCompileSetDlg::OnBnClickedButtonCompilecopysrc()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	try
+	{
+		GetDlgItem(IDC_BUTTON_COMPILECOPYSRC)->EnableWindow(FALSE);
+		BROWSEINFO bInfo;
+		bInfo.hwndOwner = GetSafeHwnd();
+		bInfo.pidlRoot = NULL;
+		bInfo.ulFlags = BIF_RETURNONLYFSDIRS;
+		memset(&bInfo, 0, sizeof(bInfo));
+
+		LPITEMIDLIST path;
+		path = SHBrowseForFolder(&bInfo);
+
+		TCHAR szpathName[300];
+		SHGetPathFromIDList(path, szpathName);
+		CString strtmp = szpathName;
+		if (strtmp != "")
+			m_strEditDestPath = szpathName;
+		else
+			m_strEditDestPath = "";
+
+		GetDlgItem(IDC_EDIT_SRCPATH)->SetWindowText(m_strEditDestPath);
+		GetDlgItem(IDC_BUTTON_COMPILECOPYSRC)->EnableWindow(TRUE);
+	}
+	catch (CException *e)
+	{
+		e->Delete();
+		AfxMessageBox(_T("경로가 잘못되었습니다 다시 선택하세요."));
+	}
+}
+
+//컴파일 복사 대상 등록
+void CCompileSetDlg::OnBnClickedRgstCompilesetBtn()
+{
+	try{
+		CString strListNm = "";
+		CString strSrcPath;
+		CString strDesPath;
+
+		m_EditListInput.GetWindowTextA(strListNm);
+		if (strListNm.GetLength() == 0)
+		{
+			AfxMessageBox(_T("리스트명 입력바랍니다."));
+			return;
+		}
+		
+		m_EditSRCPath.GetWindowTextA(strSrcPath);
+		if (strSrcPath.GetLength() == 0)
+		{
+			AfxMessageBox(_T("원본경로 입력바랍니다."));
+			return;
+		}
+
+		m_EditDestPath.GetWindowTextA(strDesPath);
+		if (strDesPath.GetLength() == 0)
+		{
+			AfxMessageBox(_T("원본경로 입력바랍니다."));
+			return;
+		}
+
+		CString strVal = strSrcPath + "," + strDesPath;
+		theApp.m_CompileListMap.SetAt(strListNm, strVal);
+		m_ComboCompileCopyList.AddString(strListNm);
+
+	}
+	catch (CException *e)
+	{
+		e->Delete();
+		AfxMessageBox(_T("처리중 오류가 발생했습니다. 재등록 바랍니다."));
+	}
 }
